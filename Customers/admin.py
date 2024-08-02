cb1e8e317e55cafd4a27ba1acf7d64a780401f5e
dzubashenko <dzubashenko@eqvantalab.ru>
2024-08-02 23:34:04 +0300

    customer admin register

--- Customers/admin.py
+++ Customers/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from models import Customer 
+
 # Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    model = Customer
+    fields = ['id', 'first_name', 'last_name', 'username']
+    readonly_fields = ['image_tag']
+    
+admin.site.register(Customer, CustomerAdmin)
+    
