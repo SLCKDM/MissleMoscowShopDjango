c9742fbde9199590667ade89c5a5af968544c636
dzubashenko <dzubashenko@eqvantalab.ru>
2024-08-02 22:53:20 +0300

    superuser creation

--- entrypoint-web.sh
+++ entrypoint-web.sh
@@ -15,7 +15,6 @@ while ! python manage.py migrate  2>&1; do
   sleep 3
 done
 # python ./backend/manage.py loaddata ./backend/recipes/TasksAPp/initial_data.json
-python manage.py createsuperuser --no-input
 # FOR DEBUG
 gunicorn MissleMoscowShopBackend.wsgi:application --bind 0.0.0.0:8000
 # python manage.py runserver 0.0.0.0:8000
@@ -26,4 +25,4 @@ fi
 if [[ -z "${DO_COLLECTSTATIC}" ]]; then
   y | python3 manage.py collectstatic
 fi
-# python manage.py createsuperuser --no-input
+python manage.py createsuperuser --no-input
