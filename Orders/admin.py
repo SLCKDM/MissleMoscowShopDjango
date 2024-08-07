658a9ebdd169177ca535e201190d2ba304328627
dzubashenko <dzubashenko@eqvantalab.ru>
2024-08-08 00:30:06 +0300

    Orders models and migrations added

--- Orders/admin.py
+++ Orders/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from Orders.models import Position, Order
+
 # Register your models here.
+class PositionAdmin(admin.ModelAdmin):
+    model = Position
+    list_display = ["product", "quantity", "price", "payed", "order"]
+    list_filter = ["product", "price", "payed", "quantity"]
+    # inlines = ["order"]
+
+
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    list_display = ["id", "name", "status", "created_at"]
+    list_filter = ["created_at"]
+    search_fields = ["name"]
+    # inlines = []
+    
+    
+admin.site.register(Position, PositionAdmin)
+admin.site.register(Order, OrderAdmin)
--- Orders/models.py
+++ Orders/models.py
@@ -1,9 +1,68 @@
-from django.db import models
 import uuid
 
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from Catalog.models import Product
+from Customers.models import Customer
 # Create your models here.
-# class Orders(models.Model):
-#     uuid = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4,
-#                             editable=False)
-#     name = models.IntegerField()
-#     user = models.ForeignKey()
+
+
+class Order(models.Model):
+    """Класс заказа.
+    """
+
+    class Status(models.TextChoices):
+        NEW = "new", _('Новый')
+        IN_PROGRESS = "in_progress", _('В процессе')
+        ON_THE_WAY = "on_the_way", _('В пути')
+        DONE = "done", _('Доставлен')
+        DECLINED = "declined", _('Отменён')
+        RETURNED = "returned", _('Возвращён')
+        
+        # @classmethod
+        # def as_set(cls):
+        #     return {
+        #         cls.NEW,
+        #         cls.IN_PROGRESS,
+        #         cls.ON_THE_WAY,
+        #         cls.DONE,
+        #         cls.DECLINED,
+        #         cls.RETURNED,
+        #     }
+    
+    
+    id = models.IntegerField(
+        primary_key=True, unique=True, editable=False, auto_created=True
+    )
+    name = models.IntegerField(unique=True, db_index=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        choices=Status.choices, default=Status.NEW, max_length=20
+    )
+    
+    def __repr__(self):
+        return f'<{self.__class__.__name__} {self.name}>'
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    """Класс позиции заказа
+    """
+    id = models.UUIDField(
+        primary_key=True, unique=True, default=uuid.uuid4, editable=False
+    )
+    quantity = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    price = models.FloatField()
+    payed = models.BooleanField(default=False)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    
+    def __repr__(self):
+        return f'<{self.__class__.__name__} {self.name}>'
+
+    def __str__(self):
+        return self.name
