c24e4705de183b253ddd31dd2c54900d33fb6dd3
dzubashenko <dzubashenko@eqvantalab.ru>
2024-08-07 23:33:34 +0300

    customer model added repr and str dunder methods

--- Customers/models.py
+++ Customers/models.py
@@ -9,3 +9,8 @@ class Customer(models.Model):
     username: str = models.CharField(max_length=50)
     language_code: str = models.CharField(max_length=5)
     
+    def __repr__(self):
+        return f'<{self.__class__.__name__} {self.name}>'
+
+    def __str__(self):
+        return self.name
