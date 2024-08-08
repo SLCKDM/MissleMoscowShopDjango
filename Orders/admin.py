2032e6d84c9acf1ec3a90bd5fa9833fbc8a5b4d3
dzubashenko <dzubashenko@eqvantalab.ru>
2024-08-08 21:31:40 +0300

    Orders admin update

--- Orders/admin.py
+++ Orders/admin.py
@@ -3,6 +3,15 @@ from django.contrib import admin
 from Orders.models import Position, Order
 
 # Register your models here.
+
+class PositionStack(admin.StackedInline):
+    exclude = ['id']
+    model = Position
+    fields = ['product.name', 'quantity', 'price', 'payed']
+    readonly_fields = ['quantity', 'price', 'payed']
+    show_change_link = False
+    extra = 0
+
 class PositionAdmin(admin.ModelAdmin):
     model = Position
     list_display = ["product", "quantity", "price", "payed", "order"]
@@ -15,7 +24,7 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "status", "created_at"]
     list_filter = ["created_at"]
     search_fields = ["name"]
-    # inlines = []
+    inlines = [PositionStack]
     
     
 admin.site.register(Position, PositionAdmin)
