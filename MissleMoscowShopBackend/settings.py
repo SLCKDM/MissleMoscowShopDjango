b3ee77a977780952583d2901a889e941a9111ffd
dzubashenko <dzubashenko@eqvantalab.ru>
2024-08-27 18:21:19 +0300

    db update

--- MissleMoscowShopBackend/settings.py
+++ MissleMoscowShopBackend/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'MissleMoscowShopBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'DB.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': getenv('DB_NAME'),
+        'USER': getenv('DB_USER'),
+        'PASSWORD': getenv('DB_PASSWORD'),
+        'HOST': getenv('DB_HOST'),
+        'PORT': getenv('DB_PORT'),
     }
 }
 
