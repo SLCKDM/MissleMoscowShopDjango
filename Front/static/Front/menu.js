e1fd6b7c3a754591ca6268a8b17915eb2416f9a8
dzubashenko <dzubashenko@eqvantalab.ru>
2024-09-01 23:00:19 +0300

    added faq

---
+++ Front/static/Front/menu.js
@@ -0,0 +1,5 @@
+function toggle() {
+    console.log('toggling!');
+    let nav_list = document.getElementById('navbar-list');
+    nav_list.style.display = nav_list.style.display === 'none' ? '':'none';
+}
