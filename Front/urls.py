6520c175f094a627464dfb4941ee0280fe7bcf80
dzubashenko <dzubashenko@eqvantalab.ru>
2025-01-09 19:24:02 +0300

    Revert "main wout dev"
    This reverts commit 917149bf4520be169ee3d2cbd742810ef16f87f4.

--- Front/urls.py
+++ Front/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'Front'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('faq', views.index, name='faq'),
     path('catalog', views.ProductListView.as_view(), name='catalog'),
     path('catalog/<str:pk>', views.ProductDetailView.as_view(), name='product'),
 ]
